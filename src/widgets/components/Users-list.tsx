32b21dcc0fdd681ed393d6e39a45bf89f9bcb44f
Ferdinand GOOR <ferdinand.goor@believe.com>
2025-07-24 14:57:36 +0200

    commit 3

--- src/widgets/components/Users-list.tsx
+++ src/widgets/components/Users-list.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+
+const API_URL = 'https://hp-api.onrender.com/api/characters';
 
 export default function UserList() {
   const [filter, setFilter] = useState('');
   const navigate = useNavigate();
 
-  const { data: users, isLoading, error } = useQuery(['users'], async () => {
-    const res = await axios.get('/api/users');
-    return res.data;
+  const useCharactersQuery = () => {
+  return useQuery({
+    queryKey: ['characters', API_URL],
+    queryFn: async (): Promise<string[]> => {
+    const response = await axios.get<string[]>(API_URL);
+    return response.data;
+},
   });
+};
+
+  const { data: users, isLoading, error } = useCharactersQuery();
 
   return (
     <div>
