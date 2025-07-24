4e4d1e7d062f1cae571a472c47465ae17cb86144
Ferdinand GOOR <ferdinand.goor@believe.com>
2025-07-24 15:01:46 +0200

    commit code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
+import UserList from './widgets/components/User_list';
 
 const App = () => (
   <Routes>
     <Route path="/about" element={<div><h1>About</h1> <p>Cette application permet de gérer les utilisateurs.</p> </div>} />
+    <Route path="/" element={<UserList />} />
   </Routes>
 )
 
